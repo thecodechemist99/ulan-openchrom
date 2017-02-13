ecb05afa89330dd17abcf8922f2ae3b931af529b
Jan Holy <holyjan3@fel.cvut.cz>
2017-02-13 16:47:39 +0100

    add validation for selected wavelenght

--- chemclipse/plugins/org.chromulan.system.control.ui/src/org/chromulan/system/control/ui/chromatogram/ChromatogramWSDPreferencePage.java
+++ chemclipse/plugins/org.chromulan.system.control.ui/src/org/chromulan/system/control/ui/chromatogram/ChromatogramWSDPreferencePage.java
@@ -16,10 +16,16 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.UpdateValueStrategy;
 import org.eclipse.core.databinding.observable.Observables;
 import org.eclipse.core.databinding.observable.map.IObservableMap;
 import org.eclipse.core.databinding.observable.map.WritableMap;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.validation.MultiValidator;
+import org.eclipse.core.databinding.validation.ValidationStatus;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.databinding.preference.PreferencePageSupport;
 import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.preference.PreferencePage;
@@ -31,46 +37,69 @@ import org.eclipse.swt.widgets.Control;
 public class ChromatogramWSDPreferencePage extends PreferencePage {
 
 	private IObservableMap<Double, Boolean> attributesMap;
+	final private DataBindingContext dbc = new DataBindingContext();
 	private ChromatogramWSDOverviewUI chromatogramWSDOverviewUI;
 
 	public ChromatogramWSDPreferencePage(ChromatogramWSDOverviewUI chromatogramWSDOverviewUI) {
 		super("Select Wave lenght");
 		this.chromatogramWSDOverviewUI = chromatogramWSDOverviewUI;
 		this.attributesMap = new WritableMap<>();
+		Map<Double, Boolean> map = chromatogramWSDOverviewUI.getChromatogramWSD().getSelectedWaveLengths();
+		attributesMap.putAll(map);
 	}
 
 	@Override
 	protected Control createContents(Composite parent) {
 
-		DataBindingContext dbc = new DataBindingContext();
+		PreferencePageSupport.create(this, dbc);
 		Composite composite = new Composite(parent, SWT.NONE);
-		Map<Double, Boolean> map = chromatogramWSDOverviewUI.getChromatogramWSD().getSelectedWaveLengths();
-		attributesMap.clear();
-		attributesMap.putAll(map);
 		Iterator<Map.Entry<Double, Boolean>> iterator = attributesMap.entrySet().iterator();
 		while(iterator.hasNext()) {
 			Button button = new Button(composite, SWT.CHECK);
 			Entry<Double, Boolean> entry = iterator.next();
 			button.setText(Double.toString(entry.getKey()));
 			IObservableValue<Boolean> observeValue = Observables.observeMapEntry(attributesMap, entry.getKey());
-			dbc.bindValue(WidgetProperties.selection().observe(button), observeValue);
+			dbc.bindValue(WidgetProperties.selection().observe(button), observeValue, new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST), null);
 		}
+		Button button = new Button(composite, SWT.PUSH);
+		button.setText("select all");
+		button.addListener(SWT.Selection, even -> {
+			attributesMap.forEach((k, v) -> attributesMap.put(k, true));
+		});
+		button = new Button(composite, SWT.PUSH);
+		button.setText("deselect all");
+		button.addListener(SWT.Selection, even -> {
+			attributesMap.forEach((k, v) -> attributesMap.put(k, false));
+		});
+		MultiValidator validator = new MultiValidator() {
+
+			@Override
+			protected IStatus validate() {
+
+				if(attributesMap.isEmpty() || attributesMap.containsValue(true)) {
+					return Status.OK_STATUS;
+				}
+				return ValidationStatus.error("Select at least one wavelenght");
+			}
+		};
+		dbc.addValidationStatusProvider(validator);
 		GridLayoutFactory.swtDefaults().numColumns(1).generateLayout(composite);
 		return parent;
 	}
 
 	@Override
+	protected void performDefaults() {
+
+		dbc.updateTargets();
+		super.performDefaults();
+	}
+
+	@Override
 	public boolean performOk() {
 
-		if(!attributesMap.containsValue(true)) {
-			return false;
-		}
-		Iterator<Entry<Double, Boolean>> iterator = attributesMap.entrySet().iterator();
+		dbc.updateModels();
 		Map<Double, Boolean> map = chromatogramWSDOverviewUI.getChromatogramWSD().getSelectedWaveLengths();
-		while(iterator.hasNext()) {
-			Entry<Double, Boolean> entry = iterator.next();
-			map.replace(entry.getKey(), entry.getValue());
-		}
+		map.putAll(attributesMap);
 		return super.performOk();
 	}
 }
