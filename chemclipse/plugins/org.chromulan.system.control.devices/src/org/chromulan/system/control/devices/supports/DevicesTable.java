40cfae1553ccad432dd3990edbab28929fdb1f85
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-01-04 11:02:19 +0100

    The copyright headers have been updated to 2018.

--- chemclipse/plugins/org.chromulan.system.control.devices/src/org/chromulan/system/control/devices/supports/DevicesTable.java
+++ chemclipse/plugins/org.chromulan.system.control.devices/src/org/chromulan/system/control/devices/supports/DevicesTable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2017 Jan Holy.
+ * Copyright (c) 2015, 2018 Jan Holy.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
